bda3fe0a55729b6cb28fc7d5cb7a9cb1c7067c8f
pedrofilgueiras <pedroffilgueiras@gmail.com>
2021-02-26 15:25:33 +0100

    update

--- R/draft_commentary.R
+++
@@ -1,6 +0,0 @@
-
-.parse_draftpick_url <-
-  function(url = "https://data.nba.net/data/5s/prod/draft/2017/draft_pick.json") {
-    data <- url %>% jsonlite::fromJSON(simplifyDataFrame = T) %>% as_tibble()
-    data
-  }
